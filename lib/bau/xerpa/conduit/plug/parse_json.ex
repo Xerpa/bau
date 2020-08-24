f73a76b40ddda1e395ded7380750b7ddc3091ff0
Thales Macedo Garitezi <thalesmg@gmail.com>
2020-08-24 11:24:37 -0300

    fix dead letter after already parsed

--- lib/bau/xerpa/conduit/plug/parse_json.ex
+++ lib/bau/xerpa/conduit/plug/parse_json.ex
@@ -7,6 +7,8 @@ defmodule Bau.Xerpa.Conduit.Plug.ParseJSON do
 
   require Logger
 
+  @parsed_flag_header "x-xerpa-bau-parsed-json"
+
   def call(message, next, opts) do
     force? = Keyword.get(opts, :force, false)
 
@@ -22,25 +24,32 @@ defmodule Bau.Xerpa.Conduit.Plug.ParseJSON do
     correlation_id = message.correlation_id
     queue = message.source
     exchange = get_header(message, "exchange")
+    # to avoid pipeline order dependencies (dead letter + parse + format)
+    already_parsed? = get_header(message, @parsed_flag_header) == "true"
 
-    case Jason.decode(message.body) do
-      {:ok, decoded} ->
-        message
-        |> put_content_type("application/json")
-        |> put_body(decoded)
-        |> next.()
+    if already_parsed? do
+      message
+    else
+      case Jason.decode(message.body) do
+        {:ok, decoded} ->
+          message
+          |> put_content_type("application/json")
+          |> put_header(@parsed_flag_header, "true")
+          |> put_body(decoded)
+          |> next.()
 
-      {:error, error} ->
-        msg = Exception.format(:error, error)
+        {:error, error} ->
+          msg = Exception.format(:error, error)
 
-        Logger.error("invalid json message. discarding.\n#{msg}",
-          request_id: request_id,
-          correlation_id: correlation_id,
-          queue: queue,
-          exchange: exchange
-        )
+          Logger.error("invalid json message. discarding.\n#{msg}",
+            request_id: request_id,
+            correlation_id: correlation_id,
+            queue: queue,
+            exchange: exchange
+          )
 
-        %{message | status: :reject}
+          %{message | status: :reject}
+      end
     end
   end
 end
