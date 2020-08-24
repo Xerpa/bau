d7d13702e41d7e6d6fcac9f7d7990784f1fca3bf
Thales Macedo Garitezi <thalesmg@gmail.com>
2020-08-24 11:36:06 -0300

    enable ci

--- mix.exs
+++ mix.exs
@@ -1,14 +1,18 @@
 defmodule Bau.MixProject do
   use Mix.Project
 
+  alias Elixir.Version
+
   def project do
+    elixir_version = Version.parse!(System.version())
+
     [
       app: :bau,
       version: "0.1.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       consolidate_protocols: Mix.env() != :test,
-      deps: deps()
+      deps: deps(elixir_version)
     ]
   end
 
@@ -16,7 +20,17 @@ defmodule Bau.MixProject do
     [extra_applications: [:logger]]
   end
 
-  defp deps do
+  defp deps(%Version{major: 1, minor: 6}) do
+    [
+      {:absinthe, ">= 1.4.0 and < 1.5.0"},
+      {:conduit, ">= 0.8.0 and < 0.13.0"},
+      {:ecto, ">= 2.2.0 and < 3.0.0"},
+      {:poison, "~> 3.1"},
+      {:tesla, ">= 1.0.0 and < 2.0.0"}
+    ]
+  end
+
+  defp deps(%Version{}) do
     [
       {:absinthe, ">= 1.4.0 and < 1.5.0"},
       {:conduit, ">= 0.8.0 and < 0.13.0"},
