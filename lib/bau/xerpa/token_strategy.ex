ef0dee5709d02d83f7ca5f72feb805233be095b3
Rafael Ramos <rramos@xerpa.com.br>
2021-08-02 19:31:56 -0300

    mix format

--- lib/bau/xerpa/token_strategy.ex
+++ lib/bau/xerpa/token_strategy.ex
@@ -1,14 +1,17 @@
-defmodule Bau.Xerpa.TokenStrategy do
-  use JokenJwks.DefaultStrategyTemplate
+if Code.ensure_loaded?(Joken) &&
+     Code.ensure_loaded?(JokenJwks) do
+  defmodule Bau.Xerpa.TokenStrategy do
+    use JokenJwks.DefaultStrategyTemplate
 
-  @moduledoc """
-  Custom JWKs URL (Xerpay Auth Platform)
-  """
+    @moduledoc """
+    Custom JWKs URL (Xerpay Auth Platform)
+    """
 
-  def init_opts(opts) do
-    url = "#{fetch_auth_iss()}/.well-known/jwks.json"
-    Keyword.merge(opts, jwks_url: url)
+    def init_opts(opts) do
+      url = "#{fetch_auth_iss()}/.well-known/jwks.json"
+      Keyword.merge(opts, jwks_url: url)
+    end
+
+    defp fetch_auth_iss, do: Application.get_env(:bau, :jwt_config)[:iss]
   end
-
-  defp fetch_auth_iss, do: Application.get_env(:bau, :jwt_config)[:iss]
 end
