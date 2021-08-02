f7788067d606df4027a76fb854d854067b91b578
Rafael Ramos <rramos@xerpa.com.br>
2021-08-01 22:08:28 -0300

    use optional deps and define module only if dependency is loaded

--- lib/bau/xerpa/tesla/middleware/request_id_forwarder.ex
+++ lib/bau/xerpa/tesla/middleware/request_id_forwarder.ex
@@ -1,15 +1,17 @@
-defmodule Bau.Xerpa.Tesla.Middleware.RequestIdForwarder do
-  @behaviour Tesla.Middleware
+if Code.ensure_loaded?(Tesla) do
+  defmodule Bau.Xerpa.Tesla.Middleware.RequestIdForwarder do
+    @behaviour Tesla.Middleware
 
-  alias Bau.Xerpa.Tracing
+    alias Bau.Xerpa.Tracing
 
-  def call(env, next, _opts) do
-    with "" <> request_id <- Tracing.get_request_id() do
-      env
-      |> Tesla.put_header("x-request-id", request_id)
-      |> Tesla.run(next)
-    else
-      _ -> Tesla.run(env, next)
+    def call(env, next, _opts) do
+      with "" <> request_id <- Tracing.get_request_id() do
+        env
+        |> Tesla.put_header("x-request-id", request_id)
+        |> Tesla.run(next)
+      else
+        _ -> Tesla.run(env, next)
+      end
     end
   end
 end
--- mix.exs
+++ mix.exs
@@ -26,21 +26,21 @@ defmodule Bau.MixProject do
 
   defp deps(%Version{major: 1, minor: 6}) do
     [
-      {:absinthe, ">= 1.4.0 and < 2.0.0"},
-      {:conduit, ">= 0.8.0 and < 0.13.0"},
-      {:ecto, ">= 2.2.0 and < 3.0.0"},
-      {:poison, "~> 3.1"},
-      {:tesla, ">= 1.0.0 and < 2.0.0"}
+      {:absinthe, ">= 1.4.0 and < 2.0.0", optional: true},
+      {:conduit, ">= 0.8.0 and < 0.13.0", optional: true},
+      {:ecto, ">= 2.2.0 and < 3.0.0", optional: true},
+      {:poison, "~> 3.1", optional: true},
+      {:tesla, ">= 1.0.0 and < 2.0.0", optional: true}
     ]
   end
 
   defp deps(%Version{}) do
     [
-      {:absinthe, ">= 1.4.0 and < 2.0.0"},
-      {:conduit, ">= 0.8.0 and < 0.13.0"},
-      {:ecto, ">= 2.2.0 and < 4.0.0"},
-      {:poison, "~> 3.1"},
-      {:tesla, ">= 1.0.0 and < 2.0.0"}
+      {:absinthe, ">= 1.4.0 and < 2.0.0", optional: true},
+      {:conduit, ">= 0.8.0 and < 0.13.0", optional: true},
+      {:ecto, ">= 2.2.0 and < 4.0.0", optional: true},
+      {:poison, "~> 3.1", optional: true},
+      {:tesla, ">= 1.0.0 and < 2.0.0", optional: true}
     ]
   end
 end
